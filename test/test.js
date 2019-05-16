45a9659115c4ba41fcc3e639d02ca31f30cb0001
Ben Miller <mail@benkmiller.com>
2019-05-16 18:29:33 -0400

    Word constructor functionality and unit tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 const assert = require('assert')
 
 const Letter = require('../letter.js')
+const Word = require('../word.js')
 
 
 describe('Letter()', function() {
@@ -26,6 +27,34 @@ describe('Letter()', function() {
       a.guess()
     }, Error)
   })
+
+})
+
+describe('Word()', function() {
+
+  
+  it('should throw an error if a guess is too long', function() {
+    assert.throws(function() {
+      let foo = new Word('foo')
+      foo.guess('ab')
+    }, Error)
+  })
+
+  it('should throw an error if a guess is too short', function() {
+    assert.throws(function() {
+      let foo = new Letter('foo')
+      foo.guess('')
+    }, Error)
+  })
+
+  it('should throw an error if guess is not defined', function() {
+    assert.throws(function() {
+      let foo = new Letter('foo')
+      foo.guess()
+    }, Error)
+  })
+
+
 
 })
 
--- word.js
+++ word.js
@@ -3,6 +3,40 @@ const Letter = require('./letter.js')
 
 
 function Word() {
+  this.letters = []
+  this.guesses = []
+
+  this.addChar = function(char) {
+    let letter = new Letter(char)
+    this.letters.push(letter)
+  }
+
+  this.addWord = function(word) {
+    word.forEach(char => {
+      this.addChar(char)
+    })
+  }
+
+  this.displayed = function(separator = ' ') {
+    let output = ''
+    this.letters.forEach(letter => {
+      output += ' ' + letter.displayed()
+    })
+    return output.trim()
+  }
+
+  this.guess = function(char) {
+    if (!this.charPicked(char)) {
+      this.letters.forEach(letter => {
+        letter.guess(char)
+      })
+      this.guesses.push(char)
+    }
+  }
+
+  this.charPicked = function(char) {
+    return this.guesses.indexOf(char) !== -1
+  }
 
 }
 
